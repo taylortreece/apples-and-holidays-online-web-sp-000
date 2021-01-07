7e331fdde7bbbb0b0242bcfd40d6a8cf92aaaad6
Taylor Treece <tayloratreece@gmail.com>
2021-01-07 19:52:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holiday|
     if holiday.values == "BBQ"
-      return season
+      return holiday
     end
   end
 end
