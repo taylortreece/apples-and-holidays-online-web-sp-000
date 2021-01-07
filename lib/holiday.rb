46d300d030e1cc9f39139e8890411bf580f99445
Taylor Treece <tayloratreece@gmail.com>
2021-01-07 19:04:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -44,8 +44,8 @@ end
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   supplies_array = []
- holiday_hash[:winter].each { |holiday, supplies| supplies_array << supplies}
- supplies_array
+  holiday_hash[:winter].each { |holiday, supplies| supplies_array << supplies}
+     supplies_array
 end
 
 def all_supplies_in_holidays(holiday_hash)
