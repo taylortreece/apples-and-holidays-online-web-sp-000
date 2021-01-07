10ba85b9e490dbb0a433744109a936709e3f9934
Taylor Treece <tayloratreece@gmail.com>
2021-01-07 19:29:18 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,8 +56,9 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
-    holiday.each do |holiday_name, supplies|
-      puts "  #{holiday_name.capitalize}: #{supplies}"
+    holiday.each do |holiday_name|
+      
+      puts "  #{holiday_name.capitalize}: #{holiday_name.values.flatten}"
     end
   end
 end
