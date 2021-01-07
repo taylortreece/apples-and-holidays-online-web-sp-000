29ca94bab5ec178e14dcfec4261eedac3a58b977
Taylor Treece <tayloratreece@gmail.com>
2021-01-07 19:22:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,7 +55,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-    puts "#{season.capitalize}: #{holiday.capitalize}"
+    puts "#{season.capitalize}: #{holiday}"
   end
 end
 
